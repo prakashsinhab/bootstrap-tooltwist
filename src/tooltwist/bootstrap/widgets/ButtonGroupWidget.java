704ed1e37f753b79fd293b9fbb6b779b4db4bb06
Richard Dimalanta <richard.dimalanta@twistresources.com>
2013-02-20 13:20:19 +0800

    ButtonGroup Widget - Initial Commit

---
+++ src/tooltwist/bootstrap/widgets/ButtonGroupWidget.java
@@ -0,0 +1,122 @@
+package tooltwist.bootstrap.widgets;
+
+import tooltwist.wbd.CodeInserter;
+import tooltwist.wbd.CodeInserterList;
+import tooltwist.wbd.StylesheetCodeInserter;
+import tooltwist.wbd.WbdException;
+import tooltwist.wbd.WbdGenerator;
+import tooltwist.wbd.WbdGenerator.GenerationMode;
+import tooltwist.wbd.WbdRadioTextProperty;
+import tooltwist.wbd.WbdRenderHelper;
+import tooltwist.wbd.WbdSizeInfo;
+import tooltwist.wbd.WbdStringProperty;
+import tooltwist.wbd.WbdWidget;
+import tooltwist.wbd.WbdWidgetController;
+
+import com.dinaa.ui.UimData;
+import com.dinaa.ui.UimHelper;
+
+
+public class ButtonGroupWidget extends WbdWidgetController {
+
+	@Override
+	public String getLabel(WbdWidget instance) throws WbdException
+	{
+		return "ButtonGroup Widget";
+	}
+
+	@Override
+	public WbdSizeInfo getSizeInfo(WbdGenerator generator, WbdWidget instance) throws WbdException
+	{
+	    return WbdSizeInfo.unknownSizeInfo();
+	}
+
+	@Override
+	protected void init(WbdWidget instance) throws WbdException
+	{
+		instance.defineProperty(new WbdStringProperty("elementId", null, "Id", ""));
+		instance.defineProperty(new WbdStringProperty("items", null, "Items", ""));
+		instance.defineProperty(new WbdRadioTextProperty("vertical", null, "Vertical", "yes,no", "no"));
+	}
+
+	@Override
+	public void renderForDesigner(WbdGenerator generator, WbdWidget instance, UimData ud, WbdRenderHelper buf) throws WbdException
+	{
+		renderWidget(generator, instance, buf);
+	}
+	
+	@Override
+	public void renderForPreview(WbdGenerator generator, WbdWidget instance, UimData ud, WbdRenderHelper buf) throws WbdException
+	{
+		renderWidget(generator, instance, buf);
+	}
+
+	@Override
+	public void renderForJSP(WbdGenerator generator, WbdWidget instance, UimHelper ud, WbdRenderHelper buf) throws WbdException
+	{
+		renderWidget(generator, instance, buf);
+	}
+
+	@Override
+	public void getCodeInserters(WbdGenerator generator, WbdWidget instance, UimData ud, CodeInserterList codeInserterList) throws WbdException {
+		GenerationMode mode = generator.getMode();
+		if (mode == GenerationMode.DESIGN)
+		{
+			// Add code inserters for design mode
+			CodeInserter[] arr = {
+				// Include a CSS snippet
+					new StylesheetCodeInserter(generator, instance, "buttonGroup_cssHeader.css")
+			};
+			codeInserterList.add(arr);
+		}
+		else if (mode == GenerationMode.PREVIEW)
+		{
+			// Add code inserters for preview mode
+			CodeInserter[] arr = {
+				// Include a CSS snippet
+					new StylesheetCodeInserter(generator, instance, "buttonGroup_cssHeader.css")
+			};
+			codeInserterList.add(arr);
+		}
+		else if (mode == GenerationMode.PRODUCTION || generator.getMode() == GenerationMode.CONTROLLER)
+		{
+			// Add code inserters for production mode
+			CodeInserter[] arr = {
+				// Include a CSS snippet
+					new StylesheetCodeInserter(generator, instance, "buttonGroup_cssHeader.css")
+			};
+			codeInserterList.add(arr);
+		}
+	}
+
+	@Override
+	public boolean widgetIsStatic(WbdWidget instance, UimData ud)
+	{
+		return true;
+	}
+	
+	private void renderWidget(WbdGenerator generator, WbdWidget instance, WbdRenderHelper buf) throws WbdException {
+		String elementId = instance.getFinalProperty(generator, "elementId");
+		String items = instance.getFinalProperty(generator, "items");
+		String vertical = instance.getFinalProperty(generator, "vertical");
+		
+		if (!elementId.equals("")) {
+			elementId = " id='" + elementId + "'";
+		}
+		
+		String verticalClass = "";
+		if (vertical.equalsIgnoreCase("yes")) {
+			verticalClass = " btn-group-vertical";
+		}
+		
+		buf.append("<div"+ elementId + " class='btn-group" + verticalClass + "'>\n");
+		
+		String[] itemList = items.split(",");
+		for (String item: itemList) {
+			buf.append("  <button class='btn'>" + item + "</button>\n");
+		}
+		
+		buf.append("</div>\n");
+	}
+	
+}
