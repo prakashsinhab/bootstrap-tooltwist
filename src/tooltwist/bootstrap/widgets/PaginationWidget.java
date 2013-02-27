5d5540a31e20711c0eb4d38cf4251b632d83728a
Ryan Cangco <ryan.cangco@twistresources.com>
2013-02-27 22:16:25 +0800

    Pagination Widget

---
+++ src/tooltwist/bootstrap/widgets/PaginationWidget.java
@@ -0,0 +1,148 @@
+package tooltwist.bootstrap.widgets;
+
+import tooltwist.wbd.CodeInserter;
+import tooltwist.wbd.CodeInserterList;
+import tooltwist.wbd.JavascriptCodeInserter;
+import tooltwist.wbd.PageImportCodeInserter;
+import tooltwist.wbd.SnippetParam;
+import tooltwist.wbd.StylesheetCodeInserter;
+import tooltwist.wbd.WbdException;
+import tooltwist.wbd.WbdGenerator;
+import tooltwist.wbd.WbdGenerator.GenerationMode;
+import tooltwist.wbd.WbdNavPointProperty;
+import tooltwist.wbd.WbdRadioTextProperty;
+import tooltwist.wbd.WbdRenderHelper;
+import tooltwist.wbd.WbdSizeInfo;
+import tooltwist.wbd.WbdStringProperty;
+import tooltwist.wbd.WbdWidget;
+import tooltwist.wbd.WbdWidgetController;
+import tooltwist.wbd.WbdProductionHelper;
+//import tooltwist.bootstrap.productionHelpers.PaginationProductionHelper;
+import com.dinaa.ui.UimData;
+import com.dinaa.ui.UimHelper;
+
+/**
+ * Pagination Widget
+ */
+public class PaginationWidget extends WbdWidgetController
+{
+	private static final String SNIPPET_PREVIEW = "pagination_preview.html";
+	private static final String SNIPPET_DESIGN = "pagination_design.html";
+	private static final String SNIPPET_PRODUCTION = "pagination_production.jsp";
+	private static final boolean USE_PRODUCTION_HELPER = false;
+
+	@Override
+	protected void init(WbdWidget instance) throws WbdException
+	{
+		instance.defineProperty(new WbdStringProperty("elementId", null, "Id", ""));
+		instance.defineProperty(new WbdRadioTextProperty("size", null, "Size", "Mini:-mini, Small:-small, Normal:-normal, Large:-large ", ""));
+		instance.defineProperty(new WbdRadioTextProperty("alignment", null, "Alignment", "Center:pagination-centered,Right:pagination-right", ""));
+//		instance.defineProperty(new WbdNavPointProperty("navpoint", null, "Navpoint", ""));
+	}
+	
+	@Override
+	public void getCodeInserters(WbdGenerator generator, WbdWidget instance, UimData ud, CodeInserterList codeInserterList) throws WbdException
+	{
+//TODO: Uncomment this as required
+		GenerationMode mode = generator.getMode();
+		if (mode == GenerationMode.DESIGN)
+		{
+			// Add code inserters for design mode
+			CodeInserter[] arr = {
+
+//				// Include a CSS snippet
+				new StylesheetCodeInserter(generator, instance, "pagination_cssHeader.css"),
+			};
+			codeInserterList.add(arr);
+		}
+		else if (mode == GenerationMode.PREVIEW)
+		{
+			// Add code inserters for preview mode
+			CodeInserter[] arr = {
+//				// Link to an external Javascript file
+//				new JavascriptLinkInserter(jsUrl),
+
+//				// Link to an external stylesheet
+//				new StylesheetLinkInserter(cssUrl),
+
+//				// Include a javascript snippet 
+//				new JavascriptCodeInserter(generator, instance, "pagination_jsHeader.js")),
+
+				// Include a CSS snippet
+				new StylesheetCodeInserter(generator, instance, "pagination_cssHeader.css"),
+			};
+			codeInserterList.add(arr);
+		}
+		else if (mode == GenerationMode.PRODUCTION || generator.getMode() == GenerationMode.CONTROLLER)
+		{
+			// Add code inserters for production mode
+			CodeInserter[] arr = {
+//				// Link to an external Javascript file
+//				new JavascriptLinkInserter(jsUrl),
+					
+//				// Link to an external stylesheet
+//				new StylesheetLinkInserter(cssUrl),
+					
+//				// Include a javascript snippet 
+//				new JavascriptCodeInserter(generator, instance, "pagination_jsHeader.js")),
+					
+				// Include a CSS snippet
+				new StylesheetCodeInserter(generator, instance, "pagination_cssHeader.css"),
+
+//				// Add import statements to the JSP
+//				new PageImportCodeInserter(XData.class.getName()),
+			};
+			codeInserterList.add(arr);
+
+			if (USE_PRODUCTION_HELPER)
+			{
+				SnippetParam[] productionHelperParams = null;
+//				codeInserterList.add(WbdProductionHelper.codeInserter(instance, PaginationProductionHelper.class.getName(), productionHelperParams));
+//				codeInserterList.add(new PageImportCodeInserter(PaginationProductionHelper.class.getName()));
+			}
+		}
+
+	}
+	
+	@Override
+	public String getLabel(WbdWidget instance) throws WbdException
+	{
+		return "Pagination";
+	}
+	
+	@Override
+	public WbdSizeInfo getSizeInfo(WbdGenerator generator, WbdWidget instance) throws WbdException
+	{
+		return WbdSizeInfo.unknownSizeInfo();
+	}
+	
+	@Override
+	public void renderForPreview(WbdGenerator generator, WbdWidget instance, UimData ud, WbdRenderHelper rh) throws WbdException
+	{
+		rh.renderSnippetForStaticPage(generator, instance, SNIPPET_PREVIEW, getSnippetParams(generator, instance, ud));
+	}
+	
+	@Override
+	public void renderForDesigner(WbdGenerator generator, WbdWidget instance, UimData ud, WbdRenderHelper rh) throws WbdException
+	{
+		rh.renderSnippetForStaticPage(generator, instance, SNIPPET_DESIGN, getSnippetParams(generator, instance, ud));
+	}
+	
+	@Override
+	public void renderForJSP(WbdGenerator generator, WbdWidget instance, UimHelper ud, WbdRenderHelper rh) throws Exception
+	{
+		rh.beforeProductionCode(generator, instance, getSnippetParams(generator, instance, ud), USE_PRODUCTION_HELPER);
+		rh.renderSnippetForProduction(generator, instance, SNIPPET_PRODUCTION);
+		rh.afterProductionCode(generator, instance);
+	}
+	
+	private SnippetParam[] getSnippetParams(WbdGenerator generator, WbdWidget instance, UimData ud) throws WbdException {
+		String size = instance.getProperty("size", null);
+		String alignment = instance.getProperty("alignment", null);
+		SnippetParam[] params = {
+			new SnippetParam("size", size),
+			new SnippetParam("alignment", alignment)
+		};
+		return params;
+	}
+}
---
+++ widgets/bootstrap/pagination/0.1/pagination_production.jsp
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<!--START-->
+<div%%idDefinition%%>
+		<div class="pagination  %%alignment%%   pagination%%size%%"     >
+			<ul>
+				<li><a href="#">Prev</a></li>
+				<li><a href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">5</a></li>
+				<li><a href="#">Next</a></li>
+			</ul>
+		</div>
+	</div>
+<!--END-->
+</body>
+</html>
