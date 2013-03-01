3f626711a28db70069a11d563701a9f2e02b1533
Ryan Cangco <ryan.cangco@twistresources.com>
2013-03-01 17:38:43 +0800

    Updates for alert widget.

--- widgets/bootstrap/alert/0.1/alert_jsHeader.js
+++ widgets/bootstrap/alert/0.1/alert_jsHeader.js
@@ -1,25 +1,57 @@
 
 var Alert = function() {
-	return {
-		myVariable: null,
-
-		init: function() {
-			alert("Alert.init()");
-
-//			// attach an event to an HTML element
-//			var self = this;
-//			jQuery(".Alert .myElementClass").click(function() {
-//				self.myMethod();
-//				// do something
-//				...
-//			});
-		},
+	
+	var args = arguments;
+	var errMsg = "";
+	
+	var alertElement = null;
+	var animate = null;
+	var timeout = null;
+	
+	if (args.length == 1) {
+		alertElement = jQuery(".alert");
 		
-		myMethod: function() {
-			alert("Alert.myMethod()");
+	}
+	else if (args.length == 2) {
+		var elementId = args[1];
+		alertElement = jQuery("#"+elementId);
+		
+	}
+	else {
+		errMsg = "Invalid arguments!";
+	}
+	
+	if (alertElement == null || alertElement.length == 0) {
+		alert(errMsg);
+	}
+	else {
+		console.log("here2", alertElement);
+		var message = args[0];
+		jQuery(alertElement).find("#message").html(message);
+		animate = jQuery(alertElement).attr("animate");
+		timeout = jQuery(alertElement).attr("timeout");
+		
+		if (animate == 'fade')
+			jQuery(alertElement).fadeIn();
+		else if (animate == 'slide')
+			jQuery(alertElement).slideDown();
+		else
+			jQuery(alertElement).show();
+		
+		if (timeout != '' && parseInt(timeout) > 0) {
+			setTimeout(function() {
+				if (animate == 'fade')
+					jQuery(alertElement).fadeOut();
+				else if (animate == 'slide')
+					jQuery(alertElement).slideUp();
+				else
+					jQuery(alertElement).hide();
+			}, timeout);
+			
 		}
-		// no comma after last method
-	};
-}();
+	}	
+	
+	
+};
 
-//jQuery(Alert.init()); // Run after page loads
+document.ready = function() {};
