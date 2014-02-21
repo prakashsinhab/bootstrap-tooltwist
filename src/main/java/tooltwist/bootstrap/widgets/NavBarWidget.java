d787bfa0bc53464764e7fd706aa17913dd85c52b
kelvinsantos <kelvin.santos@twistresources.com>
2014-02-21 16:37:14 +0800

    [BUGFIX] removed github unnecessary codes

--- src/main/java/tooltwist/bootstrap/widgets/NavBarWidget.java
+++ src/main/java/tooltwist/bootstrap/widgets/NavBarWidget.java
@@ -184,14 +184,11 @@ public class NavBarWidget extends ContainerWidget
 	public void renderForJSP(WbdGenerator generator, WbdWidget instance, UimHelper ud, WbdRenderHelper rh) throws Exception {
 		try {
 			
-<<<<<<< HEAD
-=======
 			rh.append("<%@page import=\"java.util.Arrays\"%>");
 			rh.append("<%@page import=\"java.util.List\"%>");
 			rh.append("<%@page import=\"tooltwist.cloudmall.utils.WebUtils\"%>");
 			rh.append("<%@page import=\"tooltwist.cloudmall.utils.WebUtils.SESSION_VARIABLE\"%>");
 			
->>>>>>> master
 			String rows = instance.getProperty("rows", null);
 			String brandType = instance.getProperty("brandType", null).toLowerCase();
 			String titleImagePath = instance.getProperty("titleImagePath", null);
@@ -275,13 +272,10 @@ public class NavBarWidget extends ContainerWidget
 				String linkNavpoint = instance.getProperty("linkNavpoint", wbdChildIndex);
 				String parameters = instance.getProperty("parameters", wbdChildIndex);
 				
+				Navpoint navpoint = WbdCache.findNavPoint(linkNavpoint, false);
+				
 				if (type.equals("Link")) {
 					
-<<<<<<< HEAD
-					String clazz = (currentNavpointId.equals(linkNavpoint)) ? "active" : "";
-					linkNavpoint = RoutingUIM.navpointUrl(ud, instance.getProperty("linkNavpoint", wbdChildIndex), null);
-					rh.append("<li class=\""+clazz+"\"><a href=\""+linkNavpoint + parameters+"\">"+title+"</a></li>");
-=======
 					rh.append("<% if (\""+ navpoint.getNotes() +"\".contains(WebUtils.getAttributes(request, SESSION_VARIABLE.ROLE_ID, \"\"))) { %>");
 					rh.append("");
 					rh.append("<% } else { %>");
@@ -289,7 +283,6 @@ public class NavBarWidget extends ContainerWidget
 					linkNavpoint = RoutingUIM.navpointUrl(ud, instance.getProperty("linkNavpoint", wbdChildIndex), null);
 					rh.append("<li class=\""+clazz+"\"><a href=\""+linkNavpoint + parameters+"\">"+title+"</a></li>");
 					rh.append("<% } %>");
->>>>>>> master
 					
 				} else if (type.equals("Button")) {
 					String buttonType = instance.getProperty("buttonType", wbdChildIndex);
