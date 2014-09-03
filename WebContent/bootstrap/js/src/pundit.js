72a378155cd461561aacfc1f2587acf495df6e7b
Aubrian Halili <aubrian.halili@tooltwist.com>
2014-09-03 14:29:21 +0800

    Util for retrieving Profile Photo

--- WebContent/bootstrap/js/src/pundit.js
+++ WebContent/bootstrap/js/src/pundit.js
@@ -7,3 +7,14 @@ $.validator.addMethod('completeUrl', function(value, element, param) {
   }
   return /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(value);
 }, 'Please enter a valid URL.');
+
+var util = {
+  getProfilePhoto: function(profilePhoto) {
+    if (!_.str.startsWith(profilePhoto, 'http')) {
+      var serverUrl = $('input[name="serverUrl"]').val();
+      profilePhoto = serverUrl + profilePhoto;
+    }
+    
+    return profilePhoto;
+  }
+};
