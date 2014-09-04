5eec15fe64b157423d34fd7e12f7c66be8d24426
Aubrian Halili <aubrian.halili@tooltwist.com>
2014-09-04 18:05:16 +0800

    Added Validation Methods

--- WebContent/bootstrap/js/src/pundit.js
+++ WebContent/bootstrap/js/src/pundit.js
@@ -8,6 +8,20 @@ $.validator.addMethod('completeUrl', function(value, element, param) {
   return /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(value);
 }, 'Please enter a valid URL.');
 
+$.validator.addMethod('greaterDate', function(value, element, param) {
+  var isValid = false;
+  var end = new Date(value);
+  var start = new Date(param);
+  if (_.isDate(start) && _.isDate(end)) {
+    isValid = start <= end;
+  }
+  return isValid; 
+}, 'Must be greater than {0} date.');
+
+$.validator.addMethod('greaterInt', function(value, element, param) {
+  return (_.str.toNumber(param) <= _.str.toNumber(value)); 
+}, 'Must be greater than {0} value.');
+
 var util = {
   getProfilePhoto: function(profilePhoto) {
     if (_.isNull(profilePhoto)) {
