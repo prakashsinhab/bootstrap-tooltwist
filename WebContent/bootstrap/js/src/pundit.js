740ce25fa25f8f1708a219e39cce231b7cbfbbf7
kevinbatongbakal <kevinbatongbakal@10.0.1.32>
2014-09-24 11:47:40 +0800

    optimise url validator/optimise css

--- WebContent/bootstrap/js/src/pundit.js
+++ WebContent/bootstrap/js/src/pundit.js
@@ -2,8 +2,9 @@ $.validator.addMethod('completeUrl', function(value, element, param) {
   if (value.length == 0) { return true; }
 
   if(!/^(https?|ftp):\/\//i.test(value)) {
-    value = 'http://' + value;
-    $(element).val(value);
+    if(("http://").substring(1, value.length) != value.substring(1, value.length) && ("https://").substring(1, value.length) != value.substring(1, value.length)){
+      value = 'http://' + value;
+    }$(element).val(value);
   }
   return /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(value);
 }, 'Please enter a valid URL.');
